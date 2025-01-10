c9ccc0ac79ffd5e4eb0a24afaa3a38af93b96295
DavronbekMamarasulov07 <nilufar12345davron@icloud.com>
2025-01-10 22:55:34 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
       <Navbar />
       <div className="mt-[140px] mb-[60px]">
         <Container>
-          <Card className="grid grid-cols-2 gap-x-5 gap-y-4 p-5 xl:grid-cols-3">
+          <Card className="grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-4 p-5 xl:grid-cols-3">
             {isLoading ? (
               Array.from({ length: 12 }).map((_, index) => (
                 <Skeleton
